a328705e391f38e14807674890f65399dd0bbeaa
luis <lbriones@arnet.com.ar>
2020-12-09 11:52:17 -0300

    Unidad 3

--- librosonline/vistaprevia/views.py
+++ librosonline/vistaprevia/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
      contenido= { 'nombre_sitio': 'LibrosOnline' }
-     return render(request, 'vistaprevia/index.html', contenido)
+     para_minorista = { 'tipo_usuario' : 'minorista' , 'incremento' : '25'}
+     para_mayorista = { 'tipo_usuario' : 'mayorista' , 'incremento' : '10'}
+     return render(request,
+          'vistaprevia/index.html', {'contenido':contenido,
+          'para_minorista':para_minorista,
+          'para_mayorista':para_mayorista})
+     
+def contacto(request):
+     return render(request, 'vistaprevia/contacto.html')
