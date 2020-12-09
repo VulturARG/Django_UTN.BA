14f4ff439841697d49050e9787a31eabf3984def
luis <lbriones@arnet.com.ar>
2020-12-09 13:47:39 -0300

    Backend - Unidad 1 parcial

--- librosonline/vistaprevia/admin.py
+++ librosonline/vistaprevia/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from vistaprevia.models import Producto
 
-# Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+
+    fields = ['fecha_publicacion','producto', 'ruta_imagen']
+    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen']
+    ordering = ['-fecha_publicacion']
+    list_filter = ('producto', 'fecha_publicacion')
+
+admin.site.register(Producto,ProductoAdmin)
--- librosonline/vistaprevia/models.py
+++ librosonline/vistaprevia/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    Borrador = 'Borrador'
+    Publicado = 'Publicado'
+    Retirado = 'Retirado'
+    APROBACION_PRODUCTO = (
+        (Borrador, 'Borrador'),
+        (Publicado, 'Publicado'),
+        (Retirado, 'Retirado'),
+    )
+    producto = models.CharField(max_length=200)
+    estado = models.CharField(max_length=10, choices=APROBACION_PRODUCTO, default='Borrador')
+    fecha_publicacion = models.DateTimeField('Fecha de publicación')
+    ruta_imagen = models.FileField(upload_to='fotos/%Y/%m/%d', default='defecto/defecto.png', blank=True, null=True)
+    def __str__(self):
+        return ('<%s => %s: %s, %s>' % (self.__class__.__name__, self.producto, self.fecha_publicacion, self.ruta_imagen))
